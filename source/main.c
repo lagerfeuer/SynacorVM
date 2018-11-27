005ef0a89e5ea6860e206e32e0171b629ca0cad1
Lukas Deutz <lukas.deutz@mailfence.com>
2018-11-27 21:05:17 +0100

    implemented everything, challenge.bin is stuck in endless loop

--- source/main.c
+++ source/main.c
@@ -4,8 +4,9 @@
 #define MEMORY_SIZE (((1ULL) << 15) + 8)
 #define STACK_SIZE ((1ULL) << 16)
 #define resolve(x) ((x < (1ULL << 15)) ? x : cpu->memory[x])
-#define CONTINUE 1;
-#define
+#define HALT 0
+#define CONTINUE 1
+#define MOD_VAL 32768
 
 typedef unsigned char Byte;
 typedef unsigned short Word;
@@ -62,16 +63,16 @@ unsigned int execute(CPU* cpu, Word opcode) {
   Word a, b, c;
   switch (opcode) {
   case 0: // HALT
-    return CONTINUE;
+    return HALT;
   case 1: // SET
     a = fetch(cpu);
     b = fetch(cpu);
     cpu->memory[a] = resolve(b);
-    return CONTINUE;
+    break;
   case 2: // PUSH
     a = fetch(cpu);
     cpu->stack[cpu->sp++] = resolve(a);
-    return CONTINUE;
+    break;
   case 3: // POP
     if (cpu->sp == 0) {
       printf("POP but stack is empty\n");
@@ -79,27 +80,108 @@ unsigned int execute(CPU* cpu, Word opcode) {
     }
     a = fetch(cpu);
     cpu->memory[a] = cpu->stack[cpu->sp--];
+    break;
   case 4: // EQ
     a = fetch(cpu);
     b = fetch(cpu);
     c = fetch(cpu);
     cpu->memory[a] = (resolve(b) == resolve(c)) ? 1 : 0;
-    return CONTINUE;
+    break;
   case 5: // GT
     a = fetch(cpu);
     b = fetch(cpu);
     c = fetch(cpu);
     cpu->memory[a] = (resolve(b) > resolve(c)) ? 1 : 0;
-    return CONTINUE;
+    break;
   case 6: // JMP
     a = fetch(cpu);
     cpu->pc = resolve(a);
-    return CONTINUE;
+    break;
+  case 7: // JT
+    a = fetch(cpu);
+    b = fetch(cpu);
+    if (resolve(b))
+      cpu->pc = resolve(a);
+    break;
+  case 8: // JT
+    a = fetch(cpu);
+    b = fetch(cpu);
+    if (!resolve(b))
+      cpu->pc = resolve(a);
+    break;
+  case 9: // ADD
+    a = fetch(cpu);
+    b = fetch(cpu);
+    c = fetch(cpu);
+    cpu->memory[a] = (resolve(b) + resolve(c)) % MOD_VAL;
+    break;
+  case 10: // MULT
+    a = fetch(cpu);
+    b = fetch(cpu);
+    c = fetch(cpu);
+    cpu->memory[a] = (resolve(b) * resolve(c)) % MOD_VAL;
+    break;
+  case 11: // MOD
+    a = fetch(cpu);
+    b = fetch(cpu);
+    c = fetch(cpu);
+    cpu->memory[a] = resolve(b) % resolve(c);
+    break;
+  case 12: // AND
+    a = fetch(cpu);
+    b = fetch(cpu);
+    c = fetch(cpu);
+    cpu->memory[a] = resolve(b) & resolve(c);
+    break;
+  case 13: // OR
+    a = fetch(cpu);
+    b = fetch(cpu);
+    c = fetch(cpu);
+    cpu->memory[a] = resolve(b) | resolve(c);
+    break;
+  case 14: // NOT
+    a = fetch(cpu);
+    b = fetch(cpu);
+    cpu->memory[a] = ~resolve(b);
+    break;
+  case 15: // READ
+    a = fetch(cpu);
+    b = fetch(cpu);
+    cpu->memory[a] = resolve(b);
+    break;
+  case 16: // WRITE
+    a = fetch(cpu);
+    b = fetch(cpu);
+    cpu->memory[a] = b;
+    break;
+  case 17: // CALL
+    a = fetch(cpu);
+    cpu->stack[cpu->sp++] = cpu->pc;
+    cpu->pc = a;
+    break;
+  case 18: // RET
+    if (cpu->sp == 0)
+      return HALT;
+    cpu->pc = cpu->stack[--cpu->sp];
+    break;
+  case 19: // OUT
+    a = fetch(cpu);
+    printf("%c", resolve(a));
+    break;
+  case 20: // IN
+    a = fetch(cpu);
+    int val = getchar();
+    if (val == EOF)
+      return HALT;
+    cpu->memory[a] = val;
+    break;
+  case 21: // NOOP
+    break;
   default:
     printf("Unsupported Opcode: %hu\n", opcode);
     abort();
   }
-  return 0;
+  return CONTINUE;
 }
 
 int main(int argc, char* argv[]) {
@@ -115,7 +197,7 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  while (execute(cpu, fetch(cpu)) == 0)
+  while (execute(cpu, fetch(cpu)))
     ;
 
   free(cpu);
